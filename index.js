433b49a921609e6d1d2d30b9727d4e95bee750c7
Nicolas Silva <njsilvapereira@gmail.com>
2022-11-12 03:38:49 -0300

    prueba rama nico

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ mongoose.connect(process.env.DB, options, (error) => {
     if (error) {
         console.log(error);
     } else {
-        console.log("Connected to database 👍🚀✔");
+        console.log("Connected to database 👍🚀");
     }
 })
 
